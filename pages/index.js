83b292e400f720d434c64e90effab6750fa14078
Rob Gordon <rchristopher.gordon@gmail.com>
2021-01-04 14:17:10 -0500

    Initial commit, new next

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,32 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { CssCustomProperties } from "playgoer";
+import Playgoer from "playgoer";
+import styles from "../styles/styles.json";
+import schema from "../styles/schema";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Playgoer
+      settings={{ enabled: true, public: true }}
+      styles={styles}
+      schema={schema}
+    >
+      <CssCustomProperties />
       <Head>
-        <title>Create Next App</title>
+        <title>Tone Row</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+      <main>
+        <p>
+          Tone Row lassos the digital work and collaboration of Rob Gordon{" "}
+          <a href="https://twitter.com/rob______gordon">@rob______gordon</a>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <p>
+          Sometimes an agency. Otherwise a space for research and social-impact
+          programming.
+        </p>
+        <p>Reach out to rob.gordon@tone-row.com</p>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    </Playgoer>
+  );
 }
---
+++ styles/schema.js
@@ -0,0 +1,29 @@
+const schema = {
+  color: {
+    background: { value: "#ffffff", input: { type: "color" } },
+    foreground: { value: "#000000", input: { type: "color" } },
+  },
+  spacing: {
+    mobile: { value: 15, input: { type: "range", min: 10, max: 30 } },
+    desktop: { value: 25, input: { type: "range", min: 10, max: 100 } },
+  },
+  container: {
+    mobile: { value: 300, input: { type: "range", min: 10, max: 1000 } },
+    desktop: {
+      value: 2000,
+      input: { type: "range", min: 100, max: 5000 },
+    },
+  },
+  font: {
+    size: {
+      mobile: { value: 15, input: { type: "range", min: 10, max: 30 } },
+      desktop: { value: 25, input: { type: "range", min: 10, max: 100 } },
+    },
+    lineHeight: {
+      mobile: { value: 15, input: { type: "range", min: 10, max: 30 } },
+      desktop: { value: 25, input: { type: "range", min: 10, max: 100 } },
+    },
+  },
+};
+
+export default schema;
