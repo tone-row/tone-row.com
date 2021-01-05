920b6340c878cc44d7f9c28e8249b57689548013
Rob Gordon <rchristopher.gordon@gmail.com>
2021-01-05 15:01:52 -0500

    Added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <script
+        async
+        defer
+        data-domain="tone-row.com"
+        src="https://plausible.io/js/plausible.js"
+      />
+    </>
+  );
 }
 
 export default MyApp;
