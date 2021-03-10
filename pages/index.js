85d79a035a58ad951a681b013dd1e7338085f73e
Rob Gordon <rchristopher.gordon@gmail.com>
2021-03-10 16:48:57 -0500

    Funky town

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,39 @@
 import Head from "next/head";
-import Playgoer, { CssCustomProperties } from "playgoer";
-import styles from "../styles/styles.json";
-import schema from "../styles/schema";
 
 export default function Home() {
   return (
-    <Playgoer schema={schema} styles={styles} settings={{ public: true }}>
-      <div>
-        <CssCustomProperties usePortal={false} />
-        <Head>
-          <title>Tone Row</title>
-          <link rel="icon" href="/favicon.ico" />
-          <meta
-            name="Description"
-            content="Tone Row is a digital research space & agency run by Rob Gordon"
-          />
-          <meta property="og:title" content="Tone Row" />
-          <meta
-            property="og:description"
-            content="Tone Row is a digital research space & agency run by Rob Gordon"
-          />
-          <meta property="og:image" content="https://tone-row.com/smiley.png" />
-        </Head>
-        <main>
-          <p>
-            Tone Row lassos the digital work and collaboration of Rob Gordon
-          </p>
-          <p>
-            Sometimes an agency. Otherwise a space for research and
-            social-impact programming.
-          </p>
-          <p>Reach out to rob.gordon@tone-row.com</p>
-        </main>
-      </div>
-    </Playgoer>
+    <div>
+      <Head>
+        <title>Tone Row</title>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="Description"
+          content="Tone Row is a digital research space & agency run by Rob Gordon"
+        />
+        <meta property="og:title" content="Tone Row" />
+        <meta
+          property="og:description"
+          content="Tone Row is a digital research space & agency run by Rob Gordon"
+        />
+        <meta property="og:image" content="https://tone-row.com/smiley.png" />
+      </Head>
+      <main>
+        <p>
+          Tone Row is a space for web development research with a focus on
+          programming for social-impact.
+        </p>
+        <div className="projects-list">
+          <span>
+            Our latest project is{" "}
+            <a href="https://flowchart.fun">flowchart.fun</a>
+          </span>
+        </div>
+        <p>
+          Find us on <a href="https://github.com/tone-row">Github</a> and{" "}
+          <a href="https://twitter.com/row_tone">Twitter</a>
+        </p>
+        <p>For more info reach out to rob.gordon@tone-row.com</p>
+      </main>
+    </div>
   );
 }
