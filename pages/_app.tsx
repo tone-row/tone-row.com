c767802abf15703bef0f8691c8c186770ef9fc6a
Rob Gordon <rchristopher.gordon@gmail.com>
2022-05-12 10:14:47 -0400

    feat(gtag): add GA4 tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,20 @@ import Script from "next/script";
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-N7E0DG4GJC"
+      />
+      <Script
+        id="gtag-script"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-N7E0DG4GJC');`,
+        }}
+      />
       <Head>
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="mask-icon" href="favicon.svg" color="#000000" />
